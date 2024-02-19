3fd977bbe27bd0696feaed307004fb93cce2ab01
cavilesr <carlos.aviles@globant.com>
2024-02-19 17:17:52 -0600

    Fix typo TestGreeter.java

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -3,7 +3,7 @@ package com.example;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatches.is;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertThat;
 import static org.junit.matchers.JUnitMatchers.containsString;
